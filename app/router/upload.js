5b912fbb2877c5920c25fe85f6e45c914651967b
zenxds <zenxds@gmail.com>
2022-06-06 11:30:42 +0800

    feat: use koa-body

--- app/router/upload.js
+++ app/router/upload.js
@@ -1,38 +1,10 @@
-const path = require('path')
-const fse = require('fs-extra')
-const uuid = require('uuid').v4
 const Router = require('@koa/router')
 const router = new Router()
 
-const multer = require('@koa/multer')
-const uploadDest = path.join(__dirname, '../public')
-
-// 使用multer上传
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDest)
-  },
-  filename: (req, file, cb) => {
-    const dir = uuid().split('-')[0]
-
-    fse.ensureDirSync(path.join(uploadDest, dir))
-    cb(null, path.join(dir, file.originalname))
-  },
+router.post('/', async(ctx) => {
+  // ctx.request.body
+  // ctx.request.files
+  ctx.body = JSON.stringify(ctx.request.body)
 })
-const upload = multer({
-  storage: storage,
-  fileFilter: function (req, file, cb) {
-    const ext = path.extname(file.originalname)
-    if (ext === '.zip') {
-      cb(null, true)
-    } else {
-      cb(null, false)
-    }
-  },
-})
-
-// router.post('/post', upload.single('file'), async(ctx) => {
-//   ctx.body = ctx.file.filename
-// })
 
 module.exports = router
